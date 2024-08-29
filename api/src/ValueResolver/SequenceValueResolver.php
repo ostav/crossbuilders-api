d6e65f97254a6a0254fea28899b7f49a9943d8b1
ostav <ostav.dh@gmail.com>
2024-08-29 11:17:46 +0200

    replace ValueResolver directory

--- api/src/ValueResolver/SequenceValueResolver.php
+++ api/src/ValueResolver/SequenceValueResolver.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\ValueResolver;
+namespace App\ValueResolver;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
